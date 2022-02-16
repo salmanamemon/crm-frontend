3547091a0a9679019d19a0831cb18dd58648b0f6
Salman Aziz <V-salman@damacgroup.com>
2022-02-16 15:37:12 +0400

    removed prevoius tutorial file of redux-login-simple

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
-import Login from './Components/Login';
-import Welcome from './Components/Welcome';
 
 function App() {
-  const user = useSelector(selectUser);
   return (
     <div className="App">
-      {user ? <Welcome /> : <Login />}
+      hellowwww
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 
-import store from './app/store';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>,
   document.getElementById('root')
